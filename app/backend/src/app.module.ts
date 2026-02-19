e3c08431a1a9adef420bfa3c63286ceebee177dc
jm54103 <jatuporn.mabangcru@gmail.com>
2026-02-19 21:35:26 +0700

    Dockerfile front-backend

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,23 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarketSignalsModule } from './modules/market-signals/market-signals.module';
 import { MarketSignal } from './entities/market-signal.entity';
 import { CandleSticksModule } from './modules/candle-sticks/candle-sticks.module';
 import { CandleStick } from './entities/candle-stick.entity';
 
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // หรือใส่ IP ของ DB คุณ
-      port: 5432,
-      username: 'n8n',
-      password: 'n8npass',
-      database: 'set100',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,     
       entities: [MarketSignal,CandleStick],
-      synchronize: false, // สำคัญ: เพราะคุณสร้าง Table จาก SQL เองแล้ว
+      synchronize: false, // สำคัญ: เพราะคุณสร้าง Table จาก SQL เองแล้ว ⚠️ production จริงควร false
     }),
     MarketSignalsModule, CandleSticksModule,
   ],
 })
+
+
 export class AppModule {}
