c645add3a6a2255517404852f6cd26839eaf716e
jm54103 <jatuporn.mabangcru@gmail.com>
2026-03-21 22:58:41 +0700

    export auth .sql

---
+++ sql/set100/system_parameters_202603162250.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.system_parameters (param_key,param_value,value_type,description,updated_at) VALUES
+	 ('MAX_DEVICE_SESSIONS','3','INT','จำกัดจำนวน device สูงสุด','2026-02-28 20:21:05.573886+07'),
+	 ('MAX_LOGIN_ATTEMPTS','3','INT','จำนวนครั้งที่กรอกผิดก่อนล็อก','2026-02-28 20:21:05.573886+07'),
+	 ('LOCKOUT_DURATION_MIN','5','INT','เวลาที่ล็อกบัญชี (นาที)','2026-02-28 20:21:05.573886+07'),
+	 ('SESSION_TIMEOUT_SEC','180','INT','เวลาที่เซสชันหมดอายุ (วินาที)','2026-02-28 20:21:05.573886+07'),
+	 ('SCREEN_IDLE_TIMEOUT_SEC','15','INT','เวลาที่หน้าจอว่างจากการใช้งานหมดอายุ (วินาที)','2026-03-16 22:49:04.360622+07');
