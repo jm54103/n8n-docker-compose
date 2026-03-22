a4590bbc796c9693e28bb576ad96d6f4ed5a8ce6
jm54103 <jatuporn.mabangcru@gmail.com>
2026-03-22 21:12:09 +0700

    .dockerignore

--- docker-compose-cmd.ps1
+++ docker-compose-cmd.ps1
@@ -1,4 +1,4 @@
-Remove-Item -Recurse -Force D:\DOCKER_COMPOSE\n8n\webapps\frontend\admin\node_modules
-Remove-Item -Recurse -Force D:\DOCKER_COMPOSE\n8n\webapps\backend\node_modules
+# Remove-Item -Recurse -Force D:\DOCKER_COMPOSE\n8n\webapps\frontend\admin\node_modules
+# Remove-Item -Recurse -Force D:\DOCKER_COMPOSE\n8n\webapps\backend\node_modules
 docker-compose up -d --build --force-recreate webapps
 Read-Host -Prompt "Press Enter to continue"
